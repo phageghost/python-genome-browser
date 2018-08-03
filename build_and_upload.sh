13b6e9d46b15cd0a7e13f2e178c518d42fdf86c8
phageghost <github@phageghost.net>
2018-08-03 12:10:01 -0700

    Fixed twine command line for upload to PyPi

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Did you remember to increment the version number?
 python setup.py sdist bdist_wheel
-twine upload dist/*
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
